bd682f34e47b75a1a4c60a0770ac851f1178a08a
Tyler Pewarski <tylerpewarski@Tylers-MBP.home>
2017-07-16 23:01:50 -0400

    rename length param

--- btceWrapper/getOrderBook.js
+++ btceWrapper/getOrderBook.js
@@ -1,9 +1,14 @@
 const {makeRequest} = require('./helpers');
 
-const getOrderBook = (pair, limit) => makeRequest({
+/**
+* Retrieves the active orders for a given pair
+* @param {pair} is the currencyy pair to retrieve
+* @param {depth} is the amount orders returned on each side of the order book
+*/
+const getOrderBook = (pair, depth) => makeRequest({
 	timeout: 5000,
 	strictSSL: true,
-	qs: {limit},
+	qs: {limit: depth},
 	useQueryString: true,
 	url: `https://btc-e.com/api/3/depth/${pair}`,
 	method: 'GET'
